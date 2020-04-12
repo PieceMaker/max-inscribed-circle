bd3bc1fad33af59bd7acc4a2788696d57f70c6c5
Jonathan Adams <jd.adams16@gmail.com>
2020-04-11 21:09:27 -0500

    Add scripts used to generate images.

---
+++ doc/R/medial-axis.R
@@ -0,0 +1,76 @@
+library(ggplot2)
+
+shape <- data.frame(
+  x = c(0, 1, 1, 2, 2, 3, 3, 0, 0),
+  y = c(0, 0, 3, 3, 0, 0, 4, 4, 0)
+)
+medialAxisCentral <- data.frame(
+  x = c(
+    0.5, 0.5,
+    1+cos(seq(-pi, -3/2*pi, length.out = 625))/2,
+    1, 2,
+    2+cos(seq(-3/2*pi, -2*pi, length.out = 625))/2,
+    2.5, 2.5
+  ),
+  y = c(
+    0.5, 3,
+    3+sin(seq(-pi, -3/2*pi, length.out = 625))/2,
+    3.5, 3.5,
+    3+sin(seq(-3/2*pi, -2*pi, length.out = 625))/2,
+    3, 0.5
+  )
+)
+medialAxisWings1 <- data.frame(
+  x = c(0, 0.5, 1),
+  y = c(0, 0.5, 0)
+)
+medialAxisWings2 <- data.frame(
+  x = c(2, 2.5, 3),
+  y = c(0, 0.5, 0)
+)
+medialAxisWings3 <- data.frame(
+  x = c(0, 1-(2^0.5/2)/2),
+  y = c(4, 3+(2^0.5/2)/2)
+)
+medialAxisWings4 <- data.frame(
+  x = c(2+(2^0.5/2)/2, 3),
+  y = c(3+(2^0.5/2)/2, 4)
+)
+ggplot() +
+  geom_path(data = shape, aes(x, y, linetype = 'Shape')) +
+  geom_path(
+    data = medialAxisCentral,
+    aes(x, y, linetype = 'Medial\nAxis')
+  ) +
+  geom_path(
+    data = medialAxisWings1,
+    aes(x, y, linetype = 'Medial\nAxis')
+  ) +
+  geom_path(
+    data = medialAxisWings2,
+    aes(x, y, linetype = 'Medial\nAxis')
+  ) +
+  geom_path(
+    data = medialAxisWings3,
+    aes(x, y, linetype = 'Medial\nAxis')
+  ) +
+  geom_path(
+    data = medialAxisWings4,
+    aes(x, y, linetype = 'Medial\nAxis')
+  ) +
+  coord_fixed() +
+  scale_linetype_manual(values = c("Shape" = "solid", "Medial\nAxis" = "dashed")) +
+  guides(
+    linetype = guide_legend(title = '')
+  ) +
+  theme(
+    axis.title = element_blank(),
+    axis.ticks = element_blank(),
+    axis.text = element_blank(),
+    plot.background = element_rect(fill = '#DCDCDC'),
+    legend.background = element_rect(fill = '#DCDCDC'),
+    plot.margin = margin(0.1, 0.1, 0.1, 0.1, "cm"),
+    panel.background = element_rect(fill = "#D3D3D3")
+  )
+
+ggsave(filename = 'images/medial-axis.png', height = 6, width = 6, dpi = 100)
---
+++ doc/R/motivation.R
@@ -0,0 +1,35 @@
+library(ggplot2)
+
+shape <- data.frame(
+  x = c(0, 2.5, 5, 3, 3, 2, 2, 0),
+  y = c(0, 4, 0, 4, 5, 5, 4, 0)
+)
+mic <- data.frame(
+  x = 2.5+cos(seq(0, 2*pi, length.out = 2500))/2,
+  y = 4.5+sin(seq(0, 2*pi, length.out = 2500))/2
+)
+micCenter <- data.frame(x = 2.5, y = 4.5)
+centroid <- data.frame(x = 2.5, y = 3.277778)
+ggplot(shape, aes(x, y)) +
+  geom_path() +
+  geom_path(data = mic, aes(x, y, colour = 'Maximum\nInscribed\nCircle')) +
+  geom_point(data = micCenter, aes(x, y), colour = 'blue') +
+  geom_point(data = centroid, aes(x, y, shape = "Centroid"), colour = 'red') +
+  coord_fixed() +
+  scale_colour_manual(values = c('Maximum\nInscribed\nCircle' = 'blue')) +
+  scale_shape_manual(values = c("Centroid" = 16)) +
+  guides(
+    shape = guide_legend(title = ''),
+    colour = guide_legend(title = '')
+  ) +
+  theme(
+    axis.title = element_blank(),
+    axis.ticks = element_blank(),
+    axis.text = element_blank(),
+    plot.background = element_rect(fill = '#DCDCDC'),
+    legend.background = element_rect(fill = '#DCDCDC'),
+    plot.margin = margin(0.1, 0.1, 0.1, 0.1, "cm"),
+    panel.background = element_rect(fill = "#D3D3D3")
+  )
+
+ggsave(filename = 'images/motivation.png', height = 6, width = 6, dpi = 100)
